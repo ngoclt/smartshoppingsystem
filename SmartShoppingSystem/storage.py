3e07d7885155c948f48b817dba31e24157f4430a
Ngoc LE <ngoc.le@smarp.com>
2018-05-05 13:50:20 +0300

    Fixed uploading image issue.

--- SmartShoppingSystem/storage.py
+++ SmartShoppingSystem/storage.py
@@ -4,7 +4,7 @@ import os
 
 class OverwriteStorage(FileSystemStorage):
 
-    def get_available_name(self, name):
+    def get_available_name(self, name, max_length=None):
         """Returns a filename that's free on the target storage system, and
         available for new content to be written to.
 
@@ -26,3 +26,4 @@ class OverwriteStorage(FileSystemStorage):
         if os.path.exists(self.path(name)):
             os.remove(self.path(name))
         return name
+
